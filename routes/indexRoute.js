f44dfb55b83dc7aa05d898e243e9cfa2d415e4f1
ryu820 <rhju820@naver.com>
2023-03-13 10:05:44 +0900

    좋아요 기능 업데이트

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -5,12 +5,14 @@ const postsRouter = require('./postRoute.js')
 const usersRouter = require('./userRoute.js') 
 const authRouter = require('./authRoute.js') 
 const commentsRouter = require('./commentRoute.js') 
+const likesRouter = require('./likesRoute.js')
 
 router.get("/", (req, res) => {
     res.send("안녕하세요 항해 13기 류현주입니다.");
 });
 
 router.use('/posts',[postsRouter,commentsRouter])
+router.use('/likes',[likesRouter])
 router.use('/',[usersRouter,authRouter])
 
 
---
+++ routes/likesRoute.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { Posts, Users, Likes } = require("../models");
+const { Op } = require('sequelize');
+const CustomError = require("../middlewares/errorhandler.js")
+const authmiddleware = require("../middlewares/auth-middleware.js")
+const router = express.Router();
+
+//게시글 좋아요 API
+router.post("/:postId", authmiddleware, async (req, res, next) => {
+    const { postId } = req.params;
+    const { userId } = res.locals.user;
+    try {
+        const existsLikes = await Likes.findOne({ where: { postId, userId } });
+        const existsPosts = await Posts.findOne({ where: { postId } });
+        //게시글이 없을 때
+        if (existsPosts === null) {
+            throw new CustomError("게시글이 존재하지 않습니다", 404)
+        }
+
+        if (existsLikes) {
+            await Likes.destroy({
+                where: { postId, userId }
+            })
+            return res.status(200).json({ message: "게시글의 좋아요를 취소하였습니다." })
+        } else {
+            await Likes.create({
+                UserId: userId,
+                PostId: postId,
+                likes: 1
+            })
+            return res.status(200).json({ message: "게시글의 좋아요를 등록하였습니다." })
+        }
+    } catch (err) {
+        next(err)
+    }
+
+})
+
+//좋아요 게시글 조회 API
+router.get("/", authmiddleware, async (req, res, next) => {
+    const { userId } = res.locals.user;
+    try {
+        const likes = await Likes.findAll({
+            raw: true,
+            attributes: ['Post.postId','User.userId','User.nickname','Post.title','likes','createdAt', 'updatedAt'],
+            where: { UserId: userId },
+            order: [['createdAt', 'DESC']],
+            include: [{
+                model: Users,
+                attributes: []
+            }, {
+                model: Posts,
+                attributes: [],
+            }]
+        })
+        res.json({ posts: likes });
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+module.exports = router;
